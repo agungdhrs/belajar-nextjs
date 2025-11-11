4d3c35611d7cc14e740489c7f1d29a53dc55ce35
Agung D. Saputra <generalagungdhr@gmail.com>
2025-11-11 15:25:27 +0700

    belajar nextJS merapikan kode

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,6 @@
 import Link from "next/link";
-
-async function getLastPost() {
-  const res = await fetch("https://jsonplaceholder.typicode.com/posts?_limit=5");
-  const posts = await res.json();
-  return posts;
-}
+import { getLastPost } from "@/lib/api";
+import PostsList from "@/components/PostsList";
 
 export default async function Home() {
   const posts = await getLastPost();
@@ -14,15 +10,7 @@ export default async function Home() {
         <h1>Selamat Datang di NextJS News</h1>
         <p className="subtitle">Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti minima fugiat perferendis minus maiores consequuntur neque excepturi repellat reiciendis natus.</p>
       </div>
-      <div className="post-list">
-        {
-          posts.map((post)=>(
-            <div className="post-item" key={post.id}>
-              <h2><Link href='/'>{post.title}</Link></h2>
-            </div>
-          ))
-        }
-      </div>
+      <PostsList posts={posts}/>
     </>
   );
 }
